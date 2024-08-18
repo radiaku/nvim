67728343704064adac3aa0a9fadb42d219356bed
radiaku <aku.radiaku@gmail.com>
2024-08-18 13:07:05 +0700

    adding something to keymap

--- lua/radia/plugins/telescope.lua
+++ lua/radia/plugins/telescope.lua
@@ -1,51 +1,51 @@
 return {
-  -- NOTE JIKA ERROR di windows
-  -- instal fzf error
-  --
-  -- REMEMBER you need install cmake on windows 
-  --
-  -- cd ~/AppData/Local/nvim-data/lazy/telescope-fzf-native.nvim
-  -- run this:
-  -- cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build
-  --
-  --
-  -- check build/Release
-  -- fzf.exp
-  -- fzf.lib
-  -- libfzf.dll
-  --
-  -- and done
-  -- taken from this for fixing on windows https://github.com/LunarVim/LunarVim/issues/1804	}
-  --
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    -- 	{ "nvim-telescope/telescope-fzy-native.nvim" },
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
+	-- NOTE JIKA ERROR di windows
+	-- instal fzf error
+	--
+	-- REMEMBER you need install cmake on windows
+	--
+	-- cd ~/AppData/Local/nvim-data/lazy/telescope-fzf-native.nvim
+	-- run this:
+	-- cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build
+	--
+	--
+	-- check build/Release
+	-- fzf.exp
+	-- fzf.lib
+	-- libfzf.dll
+	--
+	-- and done
+	-- taken from this for fixing on windows https://github.com/LunarVim/LunarVim/issues/1804	}
+	--
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		-- 	{ "nvim-telescope/telescope-fzy-native.nvim" },
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-    telescope.setup({
-      defaults = {
-        path_display = { "truncate" },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
-        },
-      },
-    })
+		telescope.setup({
+			defaults = {
+				path_display = { "truncate" },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+		})
 
-    -- telescope.load_extension("fzy_native")
-    telescope.load_extension("neoclip")
-    telescope.load_extension("fzf")
+		-- telescope.load_extension("fzy_native")
+		telescope.load_extension("neoclip")
+		telescope.load_extension("fzf")
+		-- telescope.load_extension("refactoring")
 
-    -- set keymaps
-    -- keymap.remove("n", "<leader>fb")
-
-  end,
+		-- set keymaps
+		-- keymap.remove("n", "<leader>fb")
+	end,
 }
