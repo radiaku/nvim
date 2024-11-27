e0eeac964b9eb249bee164cecac6d01f8e9bddeb
radiaku <aku.radiaku@gmail.com>
2024-11-27 10:02:44 +0700

    fixing telescope

--- lua/radia/plugins/telescope.lua
+++ lua/radia/plugins/telescope.lua
@@ -30,6 +30,20 @@ return {
 		telescope.setup({
 			defaults = {
 				path_display = { "truncate" },
+				layout_strategy = "horizontal",
+				layout_config = {
+					horizontal = {
+						prompt_position = "top",
+						preview_width = 0.55,
+						results_width = 0.8,
+					},
+					vertical = {
+						mirror = false,
+					},
+					width = 0.87,
+					height = 0.80,
+					preview_cutoff = 120,
+				},
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
