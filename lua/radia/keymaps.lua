f99f9683e0c1736def625ec8556c259c34f38e22
radiaku <aku.radiaku@gmail.com>
2024-12-02 23:53:45 +0700

    fixing ff

--- lua/radia/keymaps.lua
+++ lua/radia/keymaps.lua
@@ -247,8 +247,8 @@ keymap.set(
 	"<cmd>:lua require('telescope.builtin').keymaps({layout_strategy='vertical',layout_config={height=100}})<cr>",
 	{ desc = "Find Keymaps" }
 )
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files <cr>", { desc = "Fuzzy find files in cwd" })
-keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles theme=dropdown<cr>", { desc = "Fuzzy find recent files" })
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files theme=dropdown previewer=false<cr>", { desc = "Fuzzy find files in cwd" })
+keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles theme=dropdown previewer=false<cr>", { desc = "Fuzzy find recent files" })
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep <cr>", { desc = "Find string in cwd" })
 keymap.set(
 	"n",
