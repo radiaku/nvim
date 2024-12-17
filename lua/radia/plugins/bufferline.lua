f403362ec788eb5ac7e1d4c5907e8fbd43f1b009
radiaku <aku.radiaku@gmail.com>
2024-12-17 19:44:32 +0700

    fixing

--- lua/radia/plugins/bufferline.lua
+++ lua/radia/plugins/bufferline.lua
@@ -1,25 +1,25 @@
 return {
-	"akinsho/bufferline.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	version = "*",
-	config = function()
-		require("bufferline").setup({
-			options = {
-				offsets = {
-					{
-						filetype = "neo-tree",
-						text = "NeoTree",
-						separator = true,
-						text_align = "left",
-					},
-				},
-				hover = {
-					enabled = true,
-					delay = 150,
-					reveal = { "close" },
-				},
-			},
-		})
-
-	end,
+	-- "akinsho/bufferline.nvim",
+	-- dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- version = "*",
+	-- config = function()
+	-- 	require("bufferline").setup({
+	-- 		options = {
+	-- 			offsets = {
+	-- 				{
+	-- 					filetype = "neo-tree",
+	-- 					text = "NeoTree",
+	-- 					separator = true,
+	-- 					text_align = "left",
+	-- 				},
+	-- 			},
+	-- 			hover = {
+	-- 				enabled = true,
+	-- 				delay = 150,
+	-- 				reveal = { "close" },
+	-- 			},
+	-- 		},
+	-- 	})
+	--
+	-- end,
 }
