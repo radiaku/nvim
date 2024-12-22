eb75c2c5dc5de8cbc794062227bafb7443e83a82
radiaku <aku.radiaku@gmail.com>
2024-12-23 02:33:18 +0700

    fixing blink

--- lua/radia/plugins/blink.lua
+++ lua/radia/plugins/blink.lua
@@ -1,57 +1,48 @@
 return {
 	"saghen/blink.cmp",
 
-	version = "v0.*",
+	version = "v0.7.6",
 	dependencies = {
 		"L3MON4D3/LuaSnip",
-		version = "v2.*",
 		"rafamadriz/friendly-snippets", -- useful snippets
 	},
 	opts = {
+		opts_extend = { "sources.completion.enabled_providers" },
 		keymap = {
 			preset = "default",
 			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
 			["<C-e>"] = { "hide", "fallback" },
 			["<CR>"] = { "accept", "fallback" },
 
-			-- ["<Tab>"] = { "snippet_forward", "fallback" },
-			-- ["<S-Tab>"] = { "snippet_backward", "fallback" },
+			["<Tab>"] = { "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
 
-			-- ["<Up>"] = { "select_prev", "fallback" },
-			-- ["<Down>"] = { "select_next", "fallback" },
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
 			["<C-p>"] = { "select_prev", "fallback" },
 			["<C-n>"] = { "select_next", "fallback" },
 
-			-- ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-			-- ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
 		},
-
 		appearance = {
 			use_nvim_cmp_as_default = true,
 			nerd_font_variant = "mono",
 		},
-
-		-- trigger = {
-		-- 	-- When false, will not show the completion window automatically when in a snippet
-		-- 	show_in_snippet = true,
-		-- 	-- When true, will show the completion window after typing a character that matches the `keyword.regex`
-		-- 	show_on_keyword = true,
-		-- },
-
 		completion = {
 			list = {
-				max_items = 20,
+				max_items = 100,
 				selection = "auto_insert",
-				-- Controls how the completion items are selected
-				-- 'preselect' will automatically select the first item in the completion list
-				-- 'manual' will not select any item by default
-				-- 'auto_insert' will not select any item by default, and insert the completion items automatically
-				-- when selecting them
 				cycle = {
 					from_bottom = true,
 					from_top = true,
 				},
 			},
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 100,
+			},
+			-- ghost_text = { enabled = true },
 		},
 		snippets = {
 			expand = function(snippet)
@@ -68,7 +59,35 @@ return {
 			end,
 		},
 		sources = {
-			default = { "lsp", "path", "luasnip", "buffer" },
+			default = { "lsp", "luasnip", "snippets", "path", "buffer" },
+			-- providers = {
+			-- 	lsp = {
+			-- 		name = "lsp",
+			-- 		enabled = true,
+			-- 		module = "blink.cmp.sources.lsp",
+			-- 		kind = "LSP",
+			-- 		score_offset = 1000,
+			-- 	},
+			-- 	luasnip = {
+			-- 		name = "luasnip",
+			-- 		enabled = true,
+			-- 		module = "blink.cmp.sources.luasnip",
+			-- 		score_offset = 950,
+			-- 	},
+			-- 	snippets = {
+			-- 		name = "snippets",
+			-- 		min_keyword_length = 2, -- don't show when triggered manually, useful for JSON keys
+			-- 		enabled = true,
+			-- 		module = "blink.cmp.sources.snippets",
+			-- 		score_offset = 950,
+			-- 	},
+				-- buffer = {
+				-- 	fallback_for = {}, -- disable being fallback for LSP
+				-- 	max_items = 4,
+				-- 	min_keyword_length = 4,
+				-- 	score_offset = -3,
+				-- },
+			-- },
 		},
 	},
 }
