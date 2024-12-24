c41600bfc7b215c6411f83388be03f0d03618d7c
radiaku <aku.radiaku@gmail.com>
2024-12-24 17:52:17 +0700

    fixing not needed plugin

--- lua/radia/plugins/indentblankline.lua
+++ lua/radia/plugins/indentblankline.lua
@@ -1,43 +1,33 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
-	opts = {},
 	config = function()
-		local highlight = {
-			"RainbowRed",
-			"RainbowYellow",
-			"RainbowBlue",
-			"RainbowOrange",
-			"RainbowGreen",
-			"RainbowViolet",
-			"RainbowCyan",
-		}
-		local hooks = require("ibl.hooks")
-		-- create the highlight groups in the highlight setup hook, so they are reset
-		-- every time the colorscheme changes
-		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-			vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-			vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-			vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-			vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-			vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-			vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-		end)
-
-		vim.g.rainbow_delimiters = { highlight = highlight }
-		require("ibl").setup({ scope = { highlight = highlight } })
-
-		hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+		require("ibl").setup()
+		-- local highlight = {
+		-- 	"RainbowRed",
+		-- 	"RainbowYellow",
+		-- 	"RainbowBlue",
+		-- 	"RainbowOrange",
+		-- 	"RainbowGreen",
+		-- 	"RainbowViolet",
+		-- 	"RainbowCyan",
+		-- }
+		-- local hooks = require("ibl.hooks")
+		-- -- create the highlight groups in the highlight setup hook, so they are reset
+		-- -- every time the colorscheme changes
+		-- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+		-- 	vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+		-- 	vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+		-- 	vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+		-- 	vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+		-- 	vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+		-- 	vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+		-- 	vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+		-- end)
+		--
+		-- vim.g.rainbow_delimiters = { highlight = highlight }
+		-- require("ibl").setup({ scope = { highlight = highlight } })
+		--
+		-- hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
 	end,
-	-- callback = function()
-	-- 	if vim.fn.strlen(vim.fn.join(vim.fn.getline(1, "$"), "")) > 10000 then
-	-- 		local ibl = require("ibl")
-	-- 		local config = require("ibl.config")
-	-- 		ibl.setup_buffer(0, {
-	-- 			enabled = not config.get_config(0).enabled,
-	-- 		})
-	-- 		vim.fn.echo("line besar dari indent-blankline callback")
-	-- 	end
-	-- end,
 }
