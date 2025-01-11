4a09876def0f40db12fd53108c1dc830b70b988d
radiaku <aku.radiaku@gmail.com>
2025-01-11 21:06:47 +0700

    fixing gressing

---
+++ lua/radia/plugins/disabled/flutter-tools.lua
@@ -0,0 +1,9 @@
+return {
+	-- "akinsho/flutter-tools.nvim",
+	-- lazy = false,
+	-- requires = {
+	-- 	"nvim-lua/plenary.nvim",
+	-- 	"stevearc/dressing.nvim", -- optional for vim.ui.select
+	-- },
+
+}
--- lua/radia/plugins/flutter-tools.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"akinsho/flutter-tools.nvim",
-	lazy = false,
-	requires = {
-		"nvim-lua/plenary.nvim",
-		"stevearc/dressing.nvim", -- optional for vim.ui.select
-	},
-
-}
