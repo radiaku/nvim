3a1dc287ea935b71fb231eeccfe5e3ee06cf0b78
radiaku <aku.radiaku@gmail.com>
2025-04-14 03:34:46 +0700

    fixing luasnip

--- lua/radia/plugins/luasnip.lua
+++ lua/radia/plugins/luasnip.lua
@@ -1,7 +1,7 @@
 return {
 	"L3MON4D3/LuaSnip",
 	-- version = "v2.*",
-	build = vim.fn.has("win32") == 1 and "pwsh " .. vim.fn.stdpath("config") .. vim.fn.expand("/lua/radia/windows/ps_luasnip_build.ps1")
+	build = vim.fn.has("win32") == 1 and "pwsh " .. vim.fn.stdpath("config") .. vim.fn.expand("/lua/radia/windows/ps_luasnip_build_mingw.ps1")
 		or "make install_jsregexp",
 
   -- first time install
