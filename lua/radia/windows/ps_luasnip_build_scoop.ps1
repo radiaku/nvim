491b54b0bc0cb1df41ef10afa3a846f5c1ed6b10
radiaku <aku.radiaku@gmail.com>
2025-04-14 03:34:46 +0700

    fixing luasnip

--- lua/radia/windows/ps_luasnip_build_scoop.ps1
+++ lua/radia/windows/ps_luasnip_build_scoop.ps1
@@ -1,6 +1,7 @@
 # Variables
 $find = '$(CC) $(LDFLAGS) $^ $(LDLIBS) -o $@'
 $replace = '$(CC) $^ $(LDLIBS) -o $@ $(LDFLAGS)'
+$scoopRoot = Split-Path -Path (Split-Path -Path (Get-Command scoop).Source -Parent) -Parent
 
 # Function to modify Makefile
 function Modify-Makefile {
@@ -37,8 +38,8 @@ foreach ($pkg in $packages) {
 }
 
 # Paths for LuaJIT headers and libs
-$luaInc = "$env:SCOOP\apps\luajit\current\include"
-$luaLib = "$env:SCOOP\apps\luajit\current\lib"
+$luaInc = "$scoopRoot\apps\luajit\current\include"
+$luaLib = "$scoopRoot\apps\luajit\current\lib"
 
 # Set env vars for build
 $env:CC = "gcc"
@@ -49,3 +50,13 @@ $env:LDFLAGS = "-shared -L$luaLib -lluajit-5.1"
 Push-Location '.\deps\jsregexp'
 make CC=$env:CC CFLAGS="$env:CFLAGS" LDFLAGS="$env:LDFLAGS" install_jsregexp
 Pop-Location
+
+
+
+# $scoopRoot = Split-Path -Path (Split-Path -Path (Get-Command scoop).Source -Parent) -Parent
+# cd .\deps\jsregexp
+#
+# gcc -I"$scoopRoot\apps\luajit\current\include\luajit-2.1" -O2 -Wall -Wno-maybe-uninitialized -fPIC -c jsregexp.c -o jsregexp.o
+# gcc -shared jsregexp.o -o jsregexp.dll -L"$scoopRoot\apps\luajit\current\lib" -lluajit-5.1
+#
+# Write-Host "Built jsregexp.dll ✅"
