02a722e1b6a11a7b83c726ad172f55ffdc2b5814
radiaku <radiaku@gmail.com>
2025-04-20 03:24:00 +0700

    remove navic

---
+++ lua/radia/plugins/disabled/navic.lua
@@ -0,0 +1,47 @@
+return {
+	-- "SmiteshP/nvim-navic",
+ --  commit = "0088cae",
+	-- config = function()
+	-- 	require("nvim-navic").setup({
+	--
+	-- 		icons = {
+	-- 			File = " ",
+	-- 			Module = " ",
+	-- 			Namespace = "󰌗 ",
+	-- 			Package = " ",
+	-- 			Class = "󰌗 ",
+	-- 			Method = "󰆧 ",
+	-- 			Property = " ",
+	-- 			Field = " ",
+	-- 			Constructor = " ",
+	-- 			Enum = "󰕘",
+	-- 			Interface = "󰕘",
+	-- 			Function = "󰊕 ",
+	-- 			Variable = "󰆧 ",
+	-- 			Constant = "󰏿 ",
+	-- 			String = "󰀬 ",
+	-- 			Number = "󰎠 ",
+	-- 			Boolean = "◩ ",
+	-- 			Array = "󰅪 ",
+	-- 			Object = "󰅩 ",
+	-- 			Key = "󰌋 ",
+	-- 			Null = "󰟢 ",
+	-- 			EnumMember = " ",
+	-- 			Struct = "󰌗 ",
+	-- 			Event = " ",
+	-- 			Operator = "󰆕 ",
+	-- 			TypeParameter = "󰊄 ",
+	-- 		},
+	-- 		lsp = {
+	-- 			auto_attach = true,
+	-- 			preference = nil,
+	-- 		},
+	-- 		highlight = true,
+	-- 		separator = " > ",
+	-- 		depth_limit = 0,
+	-- 		depth_limit_indicator = "..",
+	-- 	})
+	--
+	-- 	vim.o.winbar = " %{%v:lua.require'nvim-navic'.get_location()%}"
+	-- end,
+}
