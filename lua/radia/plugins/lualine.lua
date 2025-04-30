298d8fee8027e52350bb9092529eb025842a205e
radiaku <aku.radiaku@gmail.com>
2025-05-01 00:33:58 +0700

    adding lsp and lualine

--- lua/radia/plugins/lualine.lua
+++ lua/radia/plugins/lualine.lua
@@ -22,7 +22,7 @@ return {
 					{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
 					{
 						"filename",
-						path = 3,
+						path = 1,
 						symbols = { modified = "  ", readonly = "", unnamed = "" },
 					},
 					{
