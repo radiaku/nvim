ad6c6f6dc342c398132d8534c1cc0b6f8f9c7816
radiaku <aku.radiaku@gmail.com>
2025-05-04 16:46:13 +0700

    temp

--- gpu_script.sh
+++ gpu_script.sh
@@ -5,6 +5,15 @@
 
 
 
-sensors | awk '/amdgpu-pci-0600/,/^$/' | awk '
-/edge:/ { printf "NVIDIA-GPU: %.0f°C", $2 }
-'
+#!/bin/bash
+
+# Extract AMD GPU Temp (from amdgpu)
+gpu_temp=$(sensors | awk '/amdgpu-pci-0600/,/^$/' | awk '/edge:/ { gsub(/\+|°C/, "", $2); printf "%.0f°C", $2 }')
+
+# Extract Dell CPU temp, Processor Fan and Motherboard Fan (from dell_smm)
+cpu_temp=$(sensors | awk '/dell_smm-isa-0000/,/^$/' | awk '/CPU:/ { gsub(/\+|°C/, "", $2); printf "%.0f°C", $2 }')
+proc_fan=$(sensors | awk '/dell_smm-isa-0000/,/^$/' | awk '/Processor Fan:/ { print $3 " RPM" }')
+mobo_fan=$(sensors | awk '/dell_smm-isa-0000/,/^$/' | awk '/Motherboard Fan:/ { print $3 " RPM" }')
+
+# Output in one line
+echo "GPU: $gpu_temp | CPU: $cpu_temp | CPU Fan: $proc_fan | MB Fan: $mobo_fan"
