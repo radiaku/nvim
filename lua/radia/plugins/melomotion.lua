e68aec8c034b623396ea970b7912d7caa14dac44
radiaku <aku.radiaku@gmail.com>
2025-06-18 00:15:43 +0700

    fixing tmux

---
+++ lua/radia/plugins/melomotion.lua
@@ -0,0 +1,6 @@
+return {
+  dir = "~/Dev/personal/melomotion",
+  name = "melomotion",
+  lazy = true,
+  cmd = { "MelomotionStart", "MelomotionStop", "MelomotionSave" }, -- Load only on these commands
+}
