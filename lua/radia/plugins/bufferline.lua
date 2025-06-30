aac589a5d373d737b5173d573214dbaa606870ce
radiaku <aku.radiaku@gmail.com>
2025-06-30 16:14:52 +0700

    try this

--- lua/radia/plugins/bufferline.lua
+++ lua/radia/plugins/bufferline.lua
@@ -5,12 +5,12 @@ return {
   config = function ()
     require('bufferline').setup()
     -- Fix bufferline when restoring a session
-    -- vim.api.nvim_create_autocmd('BufAdd', {
-    --   callback = function()
-    --     vim.schedule(function()
-    --       pcall(nvim_bufferline)
-    --     end)
-    --   end,
-    -- })
+    vim.api.nvim_create_autocmd('BufAdd', {
+      callback = function()
+        vim.schedule(function()
+          pcall(nvim_bufferline)
+        end)
+      end,
+    })
   end
 }
