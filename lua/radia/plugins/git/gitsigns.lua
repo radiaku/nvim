31ac2535fd92d1cca9f3657265d3b8a81b008bcb
radiaku <aku.radiaku@gmail.com>
2025-10-30 21:40:35 +0700

    fixing gitsigns

--- lua/radia/plugins/git/gitsigns.lua
+++ lua/radia/plugins/git/gitsigns.lua
@@ -1,5 +1,14 @@
 return {
 	"lewis6991/gitsigns.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = true,
+	config = function()
+		require("gitsigns").setup({
+			current_line_blame = true, -- show inline blame like VSCode GitLens
+			current_line_blame_opts = {
+				delay = 300,
+				virt_text_pos = "eol", -- position at end of line
+			},
+			current_line_blame_formatter = "<author>, <author_time:%R> • <summary>",
+		})
+	end,
 }
+
