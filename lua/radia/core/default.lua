4e5b58eefa08de57a1ca16050712215ad09710fb
radiaku <aku.radiaku@gmail.com>
2025-11-04 22:37:35 +0700

    try remove

--- lua/radia/core/default.lua
+++ lua/radia/core/default.lua
@@ -22,21 +22,21 @@ _G.themesname = "sonokai"
 -- vim.opt.clipboard = "unnamed"
 vim.opt.clipboard = "unnamedplus"
 
-if vim.fn.has("win32") == 1 then
-	-- Use win32yank for clipboard support
-	vim.g.clipboard = {
-		name = "win32yank-wsl",
-		copy = {
-			["+"] = "win32yank.exe -i --crlf",
-			["*"] = "win32yank.exe -i --crlf",
-		},
-		paste = {
-			["+"] = "win32yank.exe -o --lf",
-			["*"] = "win32yank.exe -o --lf",
-		},
-		cache_enabled = 0,
-	}
-end
+-- if vim.fn.has("win32") == 1 then
+-- 	-- Use win32yank for clipboard support
+-- 	vim.g.clipboard = {
+-- 		name = "win32yank-wsl",
+-- 		copy = {
+-- 			["+"] = "win32yank.exe -i --crlf",
+-- 			["*"] = "win32yank.exe -i --crlf",
+-- 		},
+-- 		paste = {
+-- 			["+"] = "win32yank.exe -o --lf",
+-- 			["*"] = "win32yank.exe -o --lf",
+-- 		},
+-- 		cache_enabled = 0,
+-- 	}
+-- end
 
 
 if vim.fn.has("win32") == 1 then
