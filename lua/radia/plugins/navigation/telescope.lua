44645f1b58c3eea1f0dcfd29dc3e6be9ca87fdba
radiaku <aku.radiaku@gmail.com>
2025-11-26 21:23:16 +0700

    fixing default map and make it we can now what happened on insert signature

--- lua/radia/plugins/navigation/telescope.lua
+++ lua/radia/plugins/navigation/telescope.lua
@@ -180,8 +180,8 @@ return {
 				},
 				mappings = {
 					i = {
-						["<C-k>"] = actions.move_selection_previous,
-						["<C-j>"] = actions.move_selection_next,
+						["<C-p>"] = actions.move_selection_previous,
+						["<C-n>"] = actions.move_selection_next,
 						["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 						["<C-Q>"] = actions.smart_send_to_qflist + actions.open_qflist,
@@ -189,8 +189,8 @@ return {
 					n = {
 						["x"] = actions.toggle_selection + actions.move_selection_better,
 						["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-						["<C-k>"] = actions.move_selection_previous,
-						["<C-j>"] = actions.move_selection_next,
+						["<C-p>"] = actions.move_selection_previous,
+						["<C-n>"] = actions.move_selection_next,
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 						["<C-Q>"] = actions.smart_send_to_qflist + actions.open_qflist,
 					},
