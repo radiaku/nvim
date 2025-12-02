80ef5bd0880115853d5560015859cd8f3f9916a6
radiaku <aku.radiaku@gmail.com>
2025-12-02 20:50:47 +0700

    fixing custom move buffer

--- lua/radia/keymaps/custom.lua
+++ lua/radia/keymaps/custom.lua
@@ -20,11 +20,11 @@ keymap.set("n", "<C-h>", ":BufferLineCyclePrev<cr>", opts)
 -- keymap.set("n", "<S-h>", ":bp<cr>", opts)
 
 
--- opts = { desc = "Move buffer to next left" }
--- keymap.set("n", "<C-l>", ":BufferLineMoveNext<CR>", opts)
---
--- opts = { desc = "Move buffer to right" }
--- keymap.set("n", "<C-h>", ":BufferLineMovePrev<CR>", opts)
+opts = { desc = "Move buffer to next left" }
+keymap.set("n", "<C-n>", ":BufferLineMoveNext<CR>", opts)
+
+opts = { desc = "Move buffer to right" }
+keymap.set("n", "<C-p>", ":BufferLineMovePrev<CR>", opts)
 
 function ClearQuickfixList()
   vim.fn.setqflist({})
