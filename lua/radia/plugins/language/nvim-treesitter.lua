fc91b6eec70a8fa642e77e1ce17d4f27dce22c54
radiaku <aku.radiaku@gmail.com>
2026-05-17 13:31:36 +0700

    ifixng tree sitter and harpoon

--- lua/radia/plugins/language/nvim-treesitter.lua
+++ lua/radia/plugins/language/nvim-treesitter.lua
@@ -1,102 +1,58 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPre", "BufNewFile" },
-		-- build = ":TSUpdate",
-		dependencies = {
-			{ "windwp/nvim-ts-autotag" },
-			{ "nvim-treesitter/nvim-treesitter-textobjects" },
-		},
+		branch = "main",
+		lazy = false,
+		build = ":TSUpdate",
 		config = function()
-			-- import nvim-treesitter plugin
-			local treesitter = require("nvim-treesitter.configs")
+			local treesitter = require("nvim-treesitter")
+			local ensure_installed = {
+				"json",
+				"javascript",
+				"html",
+				"go",
+				"php",
+				"python",
+				"css",
+				"bash",
+				"lua",
+				"vim",
+				"gitignore",
+				"query",
+			}
 
-			-- configure treesitter
 			treesitter.setup({
-				-- enable syntax highlighting
-				highlight = {
-					enable = true,
-				},
-				-- enable indentation
-				indent = { enable = true },
-				-- enable autotagging (w/ nvim-ts-autotag plugin)
-				autotag = {
-					enable = false,
-				},
-				disable = function()
-					return string.len(table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), "")) > 500000
-				end,
-				-- ensure these language parsers are installed
-				ensure_installed = {
-					"json",
-					"javascript",
-					"html",
-					"go",
-					"php",
-					"python",
-					"css",
-					"bash",
-					"lua",
-					"vim",
-					"gitignore",
-					"query",
-				},
-				incremental_selection = {
-					enable = false,
-					-- keymaps = {
-					-- 	init_selection = "<C-space>",
-					-- 	node_incremental = "<C-space>",
-					-- 	scope_incremental = false,
-					-- 	node_decremental = "<bs>",
-					-- },
-				},
-				textobjects = {
-					select = {
-						enable = true,
-
-						lookahead = true,
-
-						keymaps = {
-
-							-- ["sa"] = { query = "@attribute.outer", desc = "Select outer part of a assignment" },
-							-- ["si"] = { query = "@attribute.inner", desc = "Select inner part of a assignment" },
-							--
-							-- ["ta"] = { query = "@attribute.outer", desc = "Select outer part of a attribute" },
-							-- ["ti"] = { query = "@attribute.inner", desc = "Select inner part of a attribute" },
-
-							-- ["ga"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
-							-- ["gi"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
-
-							["ab"] = { query = "@block.outer", desc = "Select outer part of a block" },
-							["ib"] = { query = "@block.inner", desc = "Select inner part of a block" },
-
-							["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
-							["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
-
-							["ac"] = {
-								query = "@conditional.outer",
-								desc = "Select outer part of a conditional definition",
-							},
-							["ic"] = {
-								query = "@conditional.inner",
-								desc = "Select inner part of a conditional definition",
-							},
-
-							["af"] = {
-								query = "@function.outer",
-								desc = "Select outer part of a method/function definition",
-							},
-							["if"] = {
-								query = "@function.inner",
-								desc = "Select inner part of a method/function definition",
-							},
-						},
-					},
-				},
+				install_dir = vim.fn.stdpath("data") .. "/site",
 			})
 
-			-- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
-			-- require("ts_context_commentstring").setup({})
+			if #vim.api.nvim_list_uis() > 0 then
+				local installed = treesitter.get_installed("parsers")
+				local missing = vim.tbl_filter(function(lang)
+					return not vim.list_contains(installed, lang)
+				end, ensure_installed)
+				if #missing > 0 then
+					treesitter.install(missing)
+				end
+			end
+
+			local max_size = 500000
+			local start_group = vim.api.nvim_create_augroup("RadiaTreesitterStart", { clear = true })
+			vim.api.nvim_create_autocmd("FileType", {
+				group = start_group,
+				callback = function(args)
+					local bufnr = args.buf
+					if vim.bo[bufnr].buftype ~= "" then
+						return
+					end
+
+					local byte_size = vim.api.nvim_buf_get_offset(bufnr, vim.api.nvim_buf_line_count(bufnr))
+					if byte_size > max_size then
+						return
+					end
+
+					pcall(vim.treesitter.start, bufnr)
+				end,
+			})
 		end,
 	},
 }
