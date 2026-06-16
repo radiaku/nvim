5cc35fd2a2c656fe29250168d31b2670d5b939a5
radiaku <aku.radiaku@gmail.com>
2026-06-16 08:17:06 +0700

    fixing remove shada

--- lua/radia/last.lua
+++ lua/radia/last.lua
@@ -11,3 +11,18 @@ end
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
 	callback = set_filetype,
 })
+
+-- Clean up stale ShaDa temp files on startup to prevent E138
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		local shada_dir = vim.fn.stdpath("data") .. "/shada"
+		if vim.fn.isdirectory(shada_dir) == 0 then
+			return
+		end
+		for name, _ in vim.fs.dir(shada_dir) do
+			if name:match("^main%.shada%.tmp%.") then
+				vim.fn.delete(shada_dir .. "/" .. name)
+			end
+		end
+	end,
+})
